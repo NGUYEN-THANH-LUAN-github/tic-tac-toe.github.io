e576d3af3cb1cf9657a2781c047a2fd90d1570ec
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-05-03 13:39:22 +0900

    refactored, completed

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-let gameBoard = document.querySelector('[board]');;
-let grids;
-const oButton = document.querySelector('[play-as-O]');
-const xButton = document.querySelector('[play-as-X]');
+let outerBoard, grids;
+const buttons = document.querySelectorAll('[button]');
 const result = document.querySelector('[result]');
 
 result.innerText = "Please choose O or X"
-
-function createBoard() {
-    for (let i = 0; i < 9; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        div.setAttribute('grid', i);
-        gameBoard.appendChild(div);
-    }
-    grids = gameBoard.querySelectorAll('[grid]');
-    game.clearBoard();
-}
-
-
 
 Array.prototype.sample = function() {
     const index = Math.floor(Math.random() * this.length)
@@ -26,164 +11,120 @@ Array.prototype.sample = function() {
 };
 
 const game = (function() {
-    board = [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
-    ];
+    outerBoard = document.querySelector('[board]');
+    innerBoard = ["", "", "", "", "", "", "", "", ""];
     clearBoard = function() {
         grids.forEach(grid => {
             grid.classList.remove('disabled');
         });
-        this.board = [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
-        ]
+        this.innerBoard = ["", "", "", "", "", "", "", "", ""];
     };
-    checkIfFinished = function(grid) {
+    createBoard = function() {
+        for (let i = 0; i < 9; i++) {
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            div.setAttribute('grid', i);
+            outerBoard.appendChild(div);
+        }
+        grids = outerBoard.querySelectorAll('[grid]');
+        this.clearBoard();
+        this.display();
+    };
+    display = function() {
+        let i = 0;
+        grids.forEach(grid => {
+            grid.innerText = this.innerBoard[i];
+            i++;
+        })
+    };
+    computerChoose = function(chosenButtonValue) {
+        while (!this.checkIfSomeoneWon()) {
+            let sample = this.innerBoard.sample();
+            if (sample.element === "") {
+                this.innerBoard[sample.index] = chosenButtonValue === "O" ? "X" : "O";
+                if (chosenButtonValue === "O") {
+                    if (this.checkIfDraw()) {
+                        result.innerText = "Draw! Choose O or X to play again"
+                    }
+                }
+                if (this.checkIfSomeoneWon()) {
+                    result.innerText = "You Lost!";
+                }
+                this.display();
+                return;
+            }
+        }
+    };
+    playerChoose = function(chosenButtonValue) {
+        grids.forEach(grid => {
+            grid.addEventListener('click', () => {
+                if (!grid.innerText && !this.checkIfSomeoneWon()) {
+                    grid.innerText = chosenButtonValue;
+                    this.innerBoard[grid.getAttribute('grid')] = grid.innerText;
+                    if (chosenButtonValue === "X") {
+                        if (this.checkIfDraw()) {
+                            result.innerText = "Draw! Choose O or X to play again"
+                        }
+                    };
+                    if (this.checkIfSomeoneWon()) {
+                        result.innerText = "You Won!";
+                    };
+                    if (!this.checkIfDraw()) {
+                        this.computerChoose(chosenButtonValue);
+                    }
+                }
+                if (this.checkIfSomeoneWon() || this.checkIfDraw()) {
+                    grids.forEach(grid => {
+                        grid.classList.add('disabled');
+                    })
+                }
+            })
+        })
+    };
+    checkIfSomeoneWon = function() {
         if (
-            this.board[0] === this.board[1] && this.board[0] === this.board[2] && this.board[0] !== "" ||
-            this.board[3] === this.board[4] && this.board[3] === this.board[5] && this.board[3] !== "" ||
-            this.board[6] === this.board[7] && this.board[6] === this.board[8] && this.board[6] !== "" ||
-            this.board[0] === this.board[3] && this.board[0] === this.board[6] && this.board[0] !== "" ||
-            this.board[1] === this.board[4] && this.board[1] === this.board[7] && this.board[1] !== "" ||
-            this.board[2] === this.board[5] && this.board[2] === this.board[8] && this.board[2] !== "" ||
-            this.board[0] === this.board[4] && this.board[0] === this.board[8] && this.board[0] !== "" ||
-            this.board[2] === this.board[4] && this.board[2] === this.board[6] && this.board[2] !== ""
+            this.innerBoard[0] === this.innerBoard[1] && this.innerBoard[0] === this.innerBoard[2] && this.innerBoard[0] !== "" ||
+            this.innerBoard[3] === this.innerBoard[4] && this.innerBoard[3] === this.innerBoard[5] && this.innerBoard[3] !== "" ||
+            this.innerBoard[6] === this.innerBoard[7] && this.innerBoard[6] === this.innerBoard[8] && this.innerBoard[6] !== "" ||
+            this.innerBoard[0] === this.innerBoard[3] && this.innerBoard[0] === this.innerBoard[6] && this.innerBoard[0] !== "" ||
+            this.innerBoard[1] === this.innerBoard[4] && this.innerBoard[1] === this.innerBoard[7] && this.innerBoard[1] !== "" ||
+            this.innerBoard[2] === this.innerBoard[5] && this.innerBoard[2] === this.innerBoard[8] && this.innerBoard[2] !== "" ||
+            this.innerBoard[0] === this.innerBoard[4] && this.innerBoard[0] === this.innerBoard[8] && this.innerBoard[0] !== "" ||
+            this.innerBoard[2] === this.innerBoard[4] && this.innerBoard[2] === this.innerBoard[6] && this.innerBoard[2] !== ""
         ) {
             return true
         } else {
             return false
         }
-    }
+    };
     checkIfDraw = function() {
-        for (let grid of this.board) {
+        for (let grid of this.innerBoard) {
             if (grid === "") {
                 return false;
             }
         }
         return true
-    }
+    };
     return {
-        board,
+        outerBoard,
+        createBoard,
+        display,
+        checkIfSomeoneWon,
+        checkIfDraw,
         clearBoard,
-        checkIfFinished,
-        checkIfDraw
+        computerChoose,
+        playerChoose,
     }
 })();
 
-oButton.addEventListener('click', () => {
-    result.innerText = "You are playing as O. Computer plays first.";
-    gameBoard.innerHTML = "";
-    createBoard();
-    display();
-    computerChooseX();
-    display();
-    playAsO(grids);
-});
-
-xButton.addEventListener('click', () => {
-    result.innerText = "You are playing as X. You plays first."
-    gameBoard.innerHTML = "";
-    createBoard();
-    display();
-    display();
-    playAsX(grids);
-});
-
-function computerChooseX() {
-    while (!game.checkIfFinished()) {
-        let sample = game.board.sample();
-        if (sample.element === "") {
-            game.board[sample.index] = "X";
-            if (game.checkIfDraw()) {
-                result.innerText = "Draw! Choose O or X to play again"
-            }
-            if (game.checkIfFinished()) {
-                result.innerText = "You Lost!";
-            }
-            return;
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        result.innerText = `You are playing as ${button.innerText}. Computer plays first.`;
+        game.outerBoard.innerHTML = "";
+        game.createBoard();
+        if (button.innerText === "O") {
+            game.computerChoose(button.innerText);
         }
-    }
-}
-
-function computerChooseO() {
-    while (!game.checkIfFinished()) {
-        let sample = game.board.sample();
-        if (sample.element === "") {
-            game.board[sample.index] = "O";
-            if (game.checkIfFinished()) {
-                result.innerText = "You Lost!"
-            }
-            return;
-        }
-    }
-}
-
-function display() {
-    let i = 0;
-    grids.forEach(grid => {
-        grid.innerText = game.board[i];
-        i++;
-    })
-}
-
-function playAsO(grids) {
-    grids.forEach(grid => {
-        grid.addEventListener('click', () => {
-            if (!grid.innerText && !game.checkIfFinished()) {
-                grid.innerText = "O";
-                game.board[grid.getAttribute('grid')] = grid.innerText;
-                if (game.checkIfFinished()) {
-                    result.innerText = "You Won!"
-                }
-                computerChooseX();
-                display();
-            }
-            if (game.checkIfFinished() || game.checkIfDraw()) {
-                grids.forEach(grid => {
-                    grid.classList.add('disabled');
-                })
-            }
-        })
-    })
-}
-
-function playAsX(grids) {
-    grids.forEach(grid => {
-        grid.addEventListener('click', () => {
-            if (!grid.innerText && !game.checkIfFinished()) {
-                grid.innerText = "X";
-                game.board[grid.getAttribute('grid')] = grid.innerText;
-                if (game.checkIfDraw()) {
-                    result.innerText = "Draw! Choose O or X to play again"
-                }
-                if (game.checkIfFinished()) {
-                    result.innerText = "You Won!";
-                }
-                if (!game.checkIfDraw()) {
-                    computerChooseO();
-                }
-                display();
-            }
-            if (game.checkIfFinished() || game.checkIfDraw()) {
-                grids.forEach(grid => {
-                    grid.classList.add('disabled');
-                })
-            }
-        })
+        game.playerChoose(button.innerText);
     })
 }
